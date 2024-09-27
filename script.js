c61f8f2e57b92a6e481283337a474df85d5d3b09
Juan Castillo <castillojuanpablo123@gmail.com>
2024-09-26 20:19:15 -0500

    Se hace un recorrido de un arreglo

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let numero = 23
-let variasCosas = [23, true, 'Sebas', function (){}, [], numero]
-let otroArreglo = []
+// let numero = 23
+// let variasCosas = [23, true, 'Sebas', function (){}, [], numero]
+// let otroArreglo = []
 
-console.log(variasCosas[0] * variasCosas[5])
-console.log(variasCosas[1])
-console.log(variasCosas[2])
-console.log(variasCosas[3])
+// console.log(variasCosas[0] * variasCosas[5])
+// console.log(variasCosas[1])
+// console.log(variasCosas[2])
+// console.log(variasCosas[3])
+
+let numeros = [1,2,3,4,5,6,7,8,9,10]
+for(let index = 0; index <numeros.length; index++){
+    // console.log(numeros[index])
+    if (numeros[index] % 2 == 0){
+        console.log(numeros[index])
+    }
+}
