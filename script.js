f3f7b73d6b79edcf76d2224ae5ec40946ce09259
Juan Castillo <castillojuanpablo123@gmail.com>
2024-09-26 20:37:13 -0500

    arreglos

--- script.js
+++ script.js
@@ -7,10 +7,18 @@
 // console.log(variasCosas[2])
 // console.log(variasCosas[3])
 
-let numeros = [1,2,3,4,5,6,7,8,9,10]
-for(let index = 0; index <numeros.length; index++){
-    // console.log(numeros[index])
-    if (numeros[index] % 2 == 0){
-        console.log(numeros[index])
-    }
+// let numeros = [1,2,3,4,5,6,7,8,9,10]
+// for(let index = 0; index <numeros.length; index++){
+//     // console.log(numeros[index])
+//     if (numeros[index] % 2 == 0){
+//         console.log(numeros[index])
+//     }
+// }
+
+let nombres = []
+for (let index = 0; index < 5; index++) {
+    nombres[index] = prompt("Ingrese un nombre")
+}
+for (let index = 0; index < nombres.length; index++) {
+    console.log(nombres[index])
 }
